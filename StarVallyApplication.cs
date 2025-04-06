9a19252029c5d9d1d2ae9ccffd5e6955d785bcbe
LTFUST <lv2324249283@gmail.com>
2025-04-06 13:17:57 +0800

    初次提交

---
+++ StarVallyApplication.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace StarVally
+{
+    [BepInPlugin("liuheniw.csti.plugin.StarVally", "星露谷核心机制修改模组", "0.0.1")]
+    public class StarVallyApplication : BaseUnityPlugin
+    {
+
+        /**
+         * 代码执行顺序：
+         * 进入存档：
+         *      --GraphicsManager.CreateStatusGraphics
+         *      --GameManager.LoadCard
+         *      --GraphicsManager.CreateStatusGraphics
+         *      --GameManager.LoadCard
+         *      --GameManager.Awake
+         *      --GameManager.LateUpdate
+         * 重置今日：
+         *      --GraphicsManager.CreateStatusGraphics
+         *      --GameManager.LoadCard
+         *      --GraphicsManager.CreateStatusGraphics
+         *      --GameManager.LoadCard
+         *      --GameManager.Awake
+         *      --GameManager.LateUpdate
+         * 退出存档：
+         *      --GameManager.quitGame
+         *      --GameManager.LateUpdate
+         */
+
+        public static StarVallyTools starVallyTools = new StarVallyTools();
+        public static SpecialActionSet OldTimeOptions;
+        public static List<InGameStat> oldInGameStats = new List<InGameStat>();
+
+        public static string myPerk = "初始星露谷";
+        public static int oldGrandFatherCardInteractionsLength = 0;
+        public static bool hasModifyCardsAndStatus = false;
+        public static bool hasMyPerk = false;
+        //public static bool wantSetDayTime = false;
+        //存储自定义状态列表
+        public static List<string> starVallyStats = new List<string>
+        {
+            "季节",
+            "求婚",
+            "生命",
+            "能量",
+            "雨水"
+        };
+
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(StarVallyApplication));
+            Debug.Log("游戏修改模组已加载");
+        }
+
+        void Start()
+        {
+            //启动方法，可以放热键配置
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(GameManager), "Awake")]
+        public static void GameManagerAwakePostfix()
+        {
+            GameManager gm = StarVallyTools.gameManager;
+            if (gm != null)
+            {
+                //有指定特质才对游戏进行修改，主要涉及游戏基础机制类型与原版物品
+                if (hasMyPerk)
+                {
+                    if (!StarVallyTools.hasModified)
+                    {
+                        starVallyTools.LoadWeatherEffectFromAB("StarVally.springwindeffect", "SpringWindEffect", "星露谷_天气_春季_微风");
+                        starVallyTools.LoadWeatherEffectFromAB("StarVally.fallwindeffect", "FallWindEffect", "星露谷_天气_秋季_微风");
+                        StarVallyTools.hasModified = true;
+                    }
+
+
+                    ModifyGrandFather();
+
+                }
+                else
+                {
+                    if (oldGrandFatherCardInteractionsLength != 0)
+                    {
+                        RevertGrandFather();
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogError($"gm为空");
+            }
+        }
+
+        void Update()
+        {
+            //检测更新方法，可以监听热键按下并进行处理
+            //if (Input.GetKeyDown(KeyCode.Alpha9))
+            //{
+            //    wantSetDayTime = !wantSetDayTime;
+            //    Debug.LogError($"将是否修改时间设置为{wantSetDayTime}");
+            //}
+        }
+
+        //游戏加载时修改卡牌数据
+        [HarmonyPostfix, HarmonyPatch(typeof(GameManager), "LoadCard")]
+        public static void ModifyCardsAndStatus()
+        {
+            if (hasMyPerk && !hasModifyCardsAndStatus)
+            {
+                GameManager gm = StarVallyTools.gameManager;
+
+                //修改模组物品
+                if (StarVallyTools.cardDataDict.TryGetValue("星露谷_物品_单人床", out var singleBed))
+                {
+                    Debug.Log($"取得星露谷_物品_单人床卡牌");
+                    DismantleCardAction action1 = StarVallyTools.BuildAction("打盹", "星露谷_状态栏_能量", 5.625f, 5.625f, true, "星露谷_状态栏_生命", 2.083f, 2.083f, true, CardModifications.DurabilityChanges, 16, FadeToBlackTypes.Full, "打盹中...", true);
+                    DismantleCardAction action2 = StarVallyTools.BuildAction("睡觉", "星露谷_状态栏_能量", 5.625f, 5.625f, true, "星露谷_状态栏_生命", 2.083f, 2.083f, true, CardModifications.DurabilityChanges, 32, FadeToBlackTypes.Full, "睡觉中...", false);
+                    singleBed.DismantleActions = new List<DismantleCardAction> { action1, action2 };
+                }
+
+                //修改状态
+                List<InGameStat> starVallyInGameStat = new List<InGameStat>();
+                foreach (var stat in gm.AllStats)
+                {
+                    if (starVallyStats.Contains(stat.StatModel.GameName))
+                    {
+                        starVallyInGameStat.Add(stat);
+                    }
+                }
+                gm.AllStats = starVallyInGameStat;
+
+                //设置修改状态，避免反复加载重复修改
+                hasModifyCardsAndStatus = true;
+            }
+        }
+
+        //修改祖父的交互，为祖父新增剥咖啡豆及用金属废料造枪的交互动作
+        public static void ModifyGrandFather()
+        {
+            if (StarVallyTools.cardDataDict.TryGetValue("Grandfather", out var p))
+            {
+                if (oldGrandFatherCardInteractionsLength == 0)
+                {
+                    oldGrandFatherCardInteractionsLength = p.CardInteractions.Length;
+                }
+
+                {
+                    if (StarVallyTools.cardDataDict.TryGetValue("CoffeeBerries", out var a) && StarVallyTools.cardDataDict.TryGetValue("CoffeeBeans", out var b) && StarVallyTools.cardDataDict.TryGetValue("CoffeeBerryPulp", out var c))
+                    {
+                        StarVallyTools.AddCardOnCardAction(p, a, b, 1, "剥咖啡豆", 0, CardModifications.Transform, c);
+                    }
+                }
+
+                {
+                    if (StarVallyTools.cardDataDict.TryGetValue("MetalScrap", out var a) && StarVallyTools.cardDataDict.TryGetValue("Gun", out var b))
+                    {
+                        StarVallyTools.AddCardOnCardAction(p, a, b, 1, "造枪", 1);
+                    }
+                }
+            }
+        }
+
+        public static void RevertGrandFather()
+        {
+            if (StarVallyTools.cardDataDict.TryGetValue("Grandfather", out var p))
+            {
+                Array.Resize(ref p.CardInteractions,oldGrandFatherCardInteractionsLength);
+            }
+        }
+
+        //拦截主界面左侧加载的状态栏
+        [HarmonyPrefix,HarmonyPatch(typeof(GraphicsManager), "CreateStatusGraphics")]
+        public static bool GraphicsManagerCreateStatusGraphicsPrefix(StatStatus _Status, bool _Ascending)
+        {
+            Init();
+
+            if ((hasMyPerk && !starVallyStats.Contains(_Status.GameName)) || (!hasMyPerk && starVallyStats.Contains(_Status.GameName)))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //拦截卡牌点击方法
+        [HarmonyPrefix, HarmonyPatch(typeof(InGameCardBase), "OnPointerClick")]
+        public static void InGameCardBaseOnPointerClickPrefix(InGameCardBase __instance, PointerEventData _Pointer)
+        {
+            if (__instance.CardModel.CardName.DefaultText.Equals("单人床"))
+            {
+                GameManager gm = StarVallyTools.gameManager;
+                int day = gm.CurrentTickInfo.x;
+                int tick = gm.CurrentTickInfo.y;
+                int totalTick = gm.CurrentTickInfo.z;
+                int miniTick = gm.CurrentMiniTicks;
+                Debug.LogError($"当前天数为：{day}");
+                Debug.LogError($"当前时刻为：{tick}");
+                Debug.LogError($"当前总时刻为：{totalTick}");
+                Debug.LogError($"当前mini时间为：{miniTick}");
+
+                //重置时间至最近的tick（向下取整）
+                //这么做是为了清除那些只用了3分钟的动作
+                //gm.SetTimeTo(day,tick);
+                Traverse.Create(gm).Property("CurrentMiniTicks").SetValue(0);
+                Debug.LogError($"当前mini时间为：{miniTick}");
+
+                
+                    
+                CardData singleBed = __instance.CardModel;
+                    Debug.Log($"准备动手修改单人床");
+                    DismantleCardAction action1 = singleBed.DismantleActions[0];
+                    DismantleCardAction action2 = singleBed.DismantleActions[1];
+
+
+                    if (tick > 79)
+                    {
+                        //action1.AlwaysShow = false;
+                    }else
+                    {
+                        //action1.AlwaysShow = true;
+                        Traverse.Create(action1).Field("DaytimeCost").SetValue(80 - tick);
+                    }
+                    Traverse.Create(action2).Field("DaytimeCost").SetValue(104 - tick);
+
+                    singleBed.DismantleActions = new List<DismantleCardAction> {action1, action2 };
+                singleBed.s
+
+                //if (wantSetDayTime)
+                //{
+                //    Debug.LogError($"时间跃迁至次日06:00");
+                //    gm.SetTimeTo(gm.CurrentTickInfo.x + 1, 8);
+                //}
+
+                //修改单人床睡眠时间
+                //if (StarVallyTools.cardDataDict.TryGetValue("星露谷_物品_单人床", out var singleBed))
+                //{
+                //    Debug.Log($"取得星露谷_物品_单人床卡牌");
+                //    DismantleCardAction action1 = StarVallyTools.BuildAction("打盹", "星露谷_状态栏_能量", 5.625f, 5.625f, true, "星露谷_状态栏_生命", 2.083f, 2.083f, true, CardModifications.DurabilityChanges, 16, FadeToBlackTypes.Full, "打盹中...", true);
+                //    DismantleCardAction action2 = StarVallyTools.BuildAction("睡觉", "星露谷_状态栏_能量", 5.625f, 5.625f, true, "星露谷_状态栏_生命", 2.083f, 2.083f, true, CardModifications.DurabilityChanges, 32, FadeToBlackTypes.Full, "睡觉中...", false);
+                //    singleBed.DismantleActions = new List<DismantleCardAction> { action1, action2 };
+                //}
+            }
+        }
+
+        public static void Init()
+        {
+            hasMyPerk = false;
+            hasModifyCardsAndStatus = false;
+
+            if (!hasMyPerk)
+            {
+                //初始化工具类
+                starVallyTools.InitAnything();
+
+                foreach (var characterPerk in GameManager.CurrentPlayerCharacter.CharacterPerks)
+                {
+                    if (characterPerk.PerkName.DefaultText.Equals(myPerk))
+                    {
+                        hasMyPerk = true;
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ StarVallyTools.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using HarmonyLib;
+
+namespace StarVally
+{
+    public class StarVallyTools
+    {
+        public static Dictionary<string, BookmarkGroup> bookmarkGroupDict = new Dictionary<string, BookmarkGroup>();
+        public static Dictionary<string, CardData> cardDataDict = new Dictionary<string, CardData>();
+        public static Dictionary<string, CharacterPerk> characterPerkDict = new Dictionary<string, CharacterPerk>();
+        public static Dictionary<string, Encounter> encounterDict = new Dictionary<string, Encounter>();
+        public static Dictionary<string, EndgameLogCategory> endgameLogCategoryDict = new Dictionary<string, EndgameLogCategory>();
+        public static Dictionary<string, Gamemode> gamemodeDict = new Dictionary<string, Gamemode>();
+        public static Dictionary<string, GameModifierPackage> gameModifierPackageDict = new Dictionary<string, GameModifierPackage>();
+        public static Dictionary<string, GameStat> gameStatDict = new Dictionary<string, GameStat>();
+        public static Dictionary<string, LocalTickCounter> localTickCounterDict = new Dictionary<string, LocalTickCounter>();
+        public static Dictionary<string, Objective> objectiveDict = new Dictionary<string, Objective>();
+        public static Dictionary<string, PerkGroup> perkGroupDict = new Dictionary<string, PerkGroup>();
+        public static Dictionary<string, PerkTabGroup> perkTabGroupDict = new Dictionary<string, PerkTabGroup>();
+        public static Dictionary<string, PlayerCharacter> playerCharacterDict = new Dictionary<string, PlayerCharacter>();
+        public static Dictionary<string, SelfTriggeredAction> selfTriggeredActionDict = new Dictionary<string, SelfTriggeredAction>();
+
+        // GM实例
+        public static GameManager gameManager;
+        public static bool hasModified = false;
+
+
+        //初始化
+        public void InitAnything()
+        {
+            gameManager = MBSingleton<GameManager>.Instance;
+            if (cardDataDict.Count == 0)
+            {
+                Debug.Log($"===StarVallyTools开始初始化===");
+                InitCardDict();
+            }
+        }
+
+        public void InitCardDict()
+        {
+            foreach (var data in GameLoad.Instance.DataBase.AllData)
+            {
+                switch (data)
+                {
+                    case BookmarkGroup bookmarkGroup:
+                        bookmarkGroupDict.Add(bookmarkGroup.name, bookmarkGroup);
+                        break;
+                    case CardData cardData:
+                        cardDataDict.Add(cardData.name, cardData);
+                        break;
+                    case CharacterPerk characterPerk:
+                        characterPerkDict.Add(characterPerk.name, characterPerk);
+                        break;
+                    case Encounter encounter:
+                        encounterDict.Add(encounter.name, encounter);
+                        break;
+                    case EndgameLogCategory endgameLogCategory:
+                        endgameLogCategoryDict.Add(endgameLogCategory.name, endgameLogCategory);
+                        break;
+                    case Gamemode gamemode:
+                        gamemodeDict.Add(gamemode.name, gamemode);
+                        break;
+                    case GameModifierPackage gameModifierPackage:
+                        gameModifierPackageDict.Add(gameModifierPackage.name, gameModifierPackage);
+                        break;
+                    case GameStat gameStat:
+                        gameStatDict.Add(gameStat.name, gameStat);
+                        break;
+                    case LocalTickCounter localTickCounter:
+                        localTickCounterDict.Add(localTickCounter.name, localTickCounter);
+                        break;
+                    case Objective objective:
+                        objectiveDict.Add(objective.name, objective);
+                        break;
+                    case PerkGroup perkGroup:
+                        perkGroupDict.Add(perkGroup.name, perkGroup);
+                        break;
+                    case PerkTabGroup perkTabGroup:
+                        perkTabGroupDict.Add(perkTabGroup.name, perkTabGroup);
+                        break;
+                    case PlayerCharacter playerCharacter:
+                        playerCharacterDict.Add(playerCharacter.name, playerCharacter);
+                        break;
+                    case SelfTriggeredAction selfTriggeredAction:
+                        selfTriggeredActionDict.Add(selfTriggeredAction.name, selfTriggeredAction);
+                        break;
+                    default:
+                        Debug.LogError($"未知类型");
+                        break;
+                }
+            }
+            Debug.Log($"===卡牌词典已加载完成，{bookmarkGroupDict}总数为{bookmarkGroupDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{cardDataDict}总数为{cardDataDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{characterPerkDict}总数为{characterPerkDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{encounterDict}总数为{encounterDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{endgameLogCategoryDict}总数为{endgameLogCategoryDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{gamemodeDict}总数为{gamemodeDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{gameModifierPackageDict}总数为{gameModifierPackageDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{gameStatDict}总数为{gameStatDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{localTickCounterDict}总数为{localTickCounterDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{objectiveDict}总数为{objectiveDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{perkGroupDict}总数为{perkGroupDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{perkTabGroupDict}总数为{perkTabGroupDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{playerCharacterDict}总数为{playerCharacterDict.Count}===");
+            Debug.Log($"===卡牌词典已加载完成，{selfTriggeredActionDict}总数为{selfTriggeredActionDict.Count}===");
+        }
+
+        /*
+            说明：新增卡对卡交互动作（拖拽式交互结果）
+            cardData = 需要增加动作的卡片
+            triggerCard = 触发的卡片
+            productCard = 生产的卡片
+            productNumber = 生产的卡片的数量
+            actionName = 动作的名字
+            tpCost = 动作的时间
+            type = 卡片的修改类型（默认是损毁）
+            tranCard = 转换的结果（默认是null）
+         */
+        public static void AddCardOnCardAction(CardData cardData, CardData triggerCard, CardData productCard, int productNumber, string actionName, int tpCost, CardModifications type = CardModifications.Destroy, CardData tranCard = null)
+        {
+            LocalizedString text = new LocalizedString();
+            text.DefaultText = actionName;
+
+            CardDrop[] drop = new CardDrop[1];
+            drop[0] = new CardDrop(productCard, new UnityEngine.Vector2Int(productNumber, productNumber));
+
+            CardsDropCollection dropCollection = new CardsDropCollection();
+            dropCollection.SetDroppedCards(drop, false);
+            dropCollection.CollectionName = "掉落集合";
+
+            CardOnCardAction cardAction = new CardOnCardAction();
+            cardAction.GivenCardChanges.ModType = type;
+            if (type == CardModifications.Transform && tranCard != null)
+            {
+                cardAction.GivenCardChanges.TransformInto = tranCard;
+            }
+
+            Array.Resize<CardData>(ref cardAction.CompatibleCards.TriggerCards, 1);
+            cardAction.CompatibleCards.TriggerCards[0] = triggerCard;
+
+            Array.Resize<CardsDropCollection>(ref cardAction.ProducedCards, 1);
+            cardAction.ProducedCards[0] = dropCollection;
+
+            cardAction.ActionName = text;
+
+            Traverse.Create(cardAction).Field("DaytimeCost").SetValue(tpCost);
+
+            Array.Resize<CardOnCardAction>(ref cardData.CardInteractions, cardData.CardInteractions.Length + 1);
+            cardData.CardInteractions[cardData.CardInteractions.Length - 1] = cardAction;
+        }
+
+        /*
+         * 从AB包加载天气特效资源并为天气卡绑定特效
+         * 
+         * resourcePath：AB包位置，建议嵌入代码中，变量格式为“项目名.AB包名”
+         * prefabName：预制件名称，该名称为Unity中创建好的预制件的名称，用于代码从AB包中定位指定预制件
+         * cardName：卡牌名称，若为原版卡牌则为其英文名，若为模组天气，则大概率为中文名，可以通过Unity Explore查看
+         */
+        public void LoadWeatherEffectFromAB(string resourcePath, string prefabName, string cardName)
+        {
+            GameObject effectPrefab = null;
+            GameObject effectObj = null;
+            ParticleSystem particleSystem = null;
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            var effectStream = assembly.GetManifestResourceStream(resourcePath);
+            if (effectStream == null)
+            {
+                Debug.LogError($"未能获取到资源流，资源路径：{resourcePath}");
+                return;
+            }
+            var effectAB = AssetBundle.LoadFromStream(effectStream);
+            if (effectAB != null)
+            {
+                effectPrefab = effectAB.LoadAsset<GameObject>(prefabName);
+                if (effectPrefab != null)
+                {
+                    effectObj = GameObject.Instantiate(effectPrefab);
+                    if (effectObj == null)
+                    {
+                        Debug.LogError($"实例化预制体 {prefabName} 失败");
+                        return;
+                    }
+
+                    effectObj.AddComponent<WeatherSpecialEffect>();
+                    if (cardDataDict.TryGetValue(cardName, out var weatherPre))
+                    {
+                        if (weatherPre != null && weatherPre.WeatherEffects != null)
+                        {
+                            foreach (var item in weatherPre.WeatherEffects.EffectsToSpawn)
+                            {
+                                Debug.Log("捕获到一只野生的特效");
+                                Debug.Log(item.name);
+                                Debug.Log(item.tag);
+                            }
+
+                            WeatherSpecialEffect[] weatherSpecialEffects = new WeatherSpecialEffect[1];
+                            WeatherSpecialEffect weatherSpecialEffect = effectObj.GetComponent<WeatherSpecialEffect>();
+                            weatherSpecialEffects[0] = weatherSpecialEffect;
+                            weatherPre.WeatherEffects.EffectsToSpawn = weatherSpecialEffects;
+                        }
+                        else
+                        {
+                            if(weatherPre == null)
+                            {
+                                Debug.LogError($"未找到名为{cardName}的卡牌数据");
+                            }
+                            else
+                            {
+                                Debug.LogError($"名为{cardName}的卡牌数据中WeatherEffects为空");
+
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError($"卡牌词典中不存在名为 {cardName} 的卡牌");
+                    }
+
+                    particleSystem = effectObj.GetComponent<ParticleSystem>();
+                    if (particleSystem != null)
+                    {
+                        particleSystem.Stop();
+                    }
+                    else
+                    {
+                        Debug.LogError($"{prefabName}获取物理系统失败");
+                    }
+                }
+                else
+                {
+                    Debug.LogError($"加载预制体{prefabName}失败");
+                }
+            }
+            else
+            {
+                Debug.LogError($"加载AB包{resourcePath}失败");
+            }
+        }
+
+
+        /**
+         * 弹框数据结构示意：
+         * --DismantleCardAction 点击动作
+         *      --ActionName                LocalizedString     动作名
+         *      --DaytimeCost               int                 消耗时间（单位：Tick）
+         *      --StatModifications         StatModifier[]      状态修改列表
+         *          --StatModification1      StatModifier        状态修改列表项
+         *              --Stat              GameStat            状态
+         *              --ValueModifier     Vector2             修改值
+         *              --ApplyEachTick     bool                是否每Tick生效
+         *      --ReceivingCardChanges      CardStateChange     状态修改方式
+         *      --FadeToBlack               FadeToBlackTypes    遮蔽方式
+         *      --FadeMessage               LocalizedString     遮蔽提示文字
+         *      --cancelAble                bool                可取消
+         */
+        public static DismantleCardAction BuildAction(string actionName,string statName1, float valueModifierX1, float valueModifierY1, bool applyEachTick1, string statName2, float valueModifierX2, float valueModifierY2, bool applyEachTick2, CardModifications modType,int daytimeCost, FadeToBlackTypes fadeToBlack,string fadeMessage,bool cancelAble)
+        {
+            //ActionName
+            LocalizedString ActionName = new LocalizedString();
+            ActionName.DefaultText = actionName;
+
+            //StatModifications
+            StatModifier StatModification1 = new StatModifier();
+            if (StarVallyTools.gameStatDict.TryGetValue(statName1, out var stat1))
+            {
+                Debug.Log($"取得{statName1}状态");
+                StatModification1.Stat = stat1;
+            }
+            StatModification1.ValueModifier = new Vector2(valueModifierX1, valueModifierY1);
+            StatModification1.ApplyEachTick = applyEachTick1;
+
+            StatModifier StatModification2 = new StatModifier();
+            if (StarVallyTools.gameStatDict.TryGetValue(statName2, out var stat2))
+            {
+                Debug.Log($"取得{statName2}状态");
+                StatModification2.Stat = stat2;
+            }
+            StatModification2.ValueModifier = new Vector2(valueModifierX2, valueModifierY2);
+            StatModification2.ApplyEachTick = applyEachTick2;
+
+            StatModifier[] StatModifications = new StatModifier[] { StatModification1, StatModification2 };
+
+            //ReceivingCardChanges
+            CardStateChange ReceivingCardChanges = new CardStateChange();
+            ReceivingCardChanges.ModType = modType;
+
+            DismantleCardAction action = new DismantleCardAction();
+            //DaytimeCost
+            Traverse.Create(action).Field("DaytimeCost").SetValue(daytimeCost);
+
+            action.ActionName = ActionName;
+            action.StatModifications = StatModifications;
+            action.ReceivingCardChanges = ReceivingCardChanges;
+
+            //设置遮蔽
+            action.FadeToBlack = fadeToBlack;
+            LocalizedString FadeMessage = new LocalizedString();
+            FadeMessage.DefaultText = fadeMessage;
+            action.FadeMessage = FadeMessage;
+            action.Cancellable = cancelAble;
+
+            return action;
+        }
+
+    }
+}
